e39a77f8dd8f73f66a7a7dcecb070eecf899e6d8
nbaonguyen111 <baonguyenmiennam@gmail.com>
2025-08-02 16:05:38 +0700

    Reset main branch to empty state

--- tg-dienmay/.next/static/chunks/src_app_admin_quan-tri-vien_page_jsx_df8f020f._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_53f2570a._.js"
-],
-    source: "dynamic"
-});
--- tg-dienmay/.next/static/chunks/src_app_dang-nhap_page_jsx_4954f38e._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_a33e62c8._.js"
-],
-    source: "dynamic"
-});
